8c1391a0788a6e7445f19693105c55ee285d0f28
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-08-14 15:30:28 -0700

    THANK YOU LANCE I got the random color thing to work, aaaaaaaaaaaaaaaaaaa

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,10 @@ import Header from './Header';
 // import PhotoAlbum from './PhotoAlbum';
 // import Button from './Button';
 import Footer from './Footer';
+import RandomColor from './RandomColor';
 // import Color from './Color';
-import ColorPicker from '../components/ColorPicker';
-import ColorDisplay from './ColorDisplay';
+// import ColorPicker from '../components/ColorPicker';
+// import ColorDisplay from './ColorDisplay';
 // import Incrementer from '../components/Incrementer';
 
 
@@ -17,27 +18,28 @@ const textProps = {
 
 export default class App extends Component {
 
-    state = {
-        color: ''
-    }
+    // state = {
+    //     color: ''
+    // }
     
-    changeColor = color => {
-        console.log(color);
-        this.setState({ color });
-    }
+    // changeColor = color => {
+    //     console.log(color);
+    //     this.setState({ color });
+    // }
 
     render() {
 
-        const { color } = this.state;
+        // const { color } = this.state;
 
         return (<> 
         <Header header={textProps.header} />
         {/* <PhotoAlbum photos={textProps.photos} /> */}
         <Footer footer={textProps.footer} />
         {/* <Color name="PANK" hex="#00aa00" rgb={[200, 10, 20]}/> */}
-        <ColorPicker changeColorHandler={this.changeColor} />
-        <ColorDisplay color={color} />
+        {/* <ColorPicker changeColorHandler={this.changeColor} />
+        <ColorDisplay color={color} /> */}
         {/* { <Incrementer /> } */}
+        <RandomColor />
     </>);
     }
 }
---
+++ src/components/RandomColor.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class RandomColor extends Component {
+    state = {
+        color: 'white'
+    }
+
+    componentDidMount() {
+        setInterval(() => { 
+            const colors = ['purple', 'red', 'green', 'pink', 'white', 'yellow', 'orange', 'blue', 'cyan', 'magenta'];
+            const num = Math.floor(Math.random() * colors.length);
+            this.setState({ color: colors[num] }); 
+        }, 1000);
+    }
+
+    render() {
+        return (<> 
+        <div style={{ backgroundColor: this.state.color }}>COLOR</div>
+        </>);
+    }
+}
+
